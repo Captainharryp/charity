4afb8a09723afb1675299db6d257ae0daaf0a87f
Captain Harry <captainharri@gmail.com>
2024-06-01 14:20:53 +0100

    postgres install

--- charity/settings.py
+++ charity/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'charity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST', default='localhost'),
+        'PORT': config('DATABASE_PORT', default='5432'),
     }
 }
 
